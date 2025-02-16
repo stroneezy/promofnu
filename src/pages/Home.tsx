1215976c95ccc4e1a62cf47695a8caaff2fafc5c
stroneezy <armstrongm5580@gmail.com>
2025-02-16 02:34:17 -0700

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
                   </a>
                 </div>
                 <div className="col">
-                  <a className="card p-3">
+                  <a className="card p-3" href="/dashboard">
                     <div className="mb-4">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-layout">
                         <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
