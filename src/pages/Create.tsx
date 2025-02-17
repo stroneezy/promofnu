33c227ac080665da9bc3189bbb4220bb848e881b
stroneezy <armstrongm5580@gmail.com>
2025-02-16 22:59:12 -0700

    New updates

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,24 +1,79 @@
-// import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
-// import Layout from "../layouts/Layout";
+import Layout from "../layouts/Layout";
 
-// // The editor core
-// import type { Value } from "@react-page/editor";
-// import Editor from "@react-page/editor";
+// The editor core
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/Header";
+import Image from "@editorjs/Image";
+import Paragraph from "@editorjs/Paragraph";
+import editorJsColumns from "@calumk/editorjs-columns";
 
-// // The rich text area plugin
-// import slate from "@react-page/plugins-slate";
+const Create: React.FC = () => {
+  const editorRef = useRef<EditorJS | null>(null);
 
-// const cellPlugins = [slate()];
+  useEffect(() => {
+    console.log("in");
+    console.log(editorRef.current);
+    if (editorRef.current) {
+      console.log("in 2");
+      const editor = new EditorJS({
+        holder: "editorjs",
+        placeholder: "Let`s make a flyer!",
+        tools: {
+          header: {
+            class: Header,
+            inlineToolbar: true,
+            config: {
+              placeholder: "Enter a header",
+              levels: [1, 2, 3],
+              defaultLevel: 2,
+            },
+          },
+          image: {
+            class: Image,
+          },
+          columns: {
+            class: editorJsColumns,
+            config: {
+              tools: {
+                header: Header,
+                image: Image,
+                paragraph: Paragraph
+              },
+              EditorJsLibrary: EditorJS, //ref EditorJS - This means only one global thing
+            },
+          },
+          paragraph: {
+            class: Paragraph,
+            inlineToolbar: true,
+          },
+        },
+        onReady: () => {
+          console.log("Editor.js is ready!");
+        },
+        onChange: () => {
+          console.log("Content changed!");
+        },
+      });
 
-// const Create: React.FC = () => {
-//   const [value, setValue] = useState<Value | null>(null);
-//   return (
-//     <Layout>
-//       <h1>Create</h1>
-//       <Editor cellPlugins={cellPlugins} value={value} onChange={setValue} />
-//     </Layout>
-//   );
-// };
+      editorRef.current = editor;
+    }
 
-// export default Create;
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.destroy();
+        editorRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Create</h1>
+      <div id="editorjs" ref={editorRef} style={{ padding: "20px", minHeight: "300px" }}></div>
+    </Layout>
+  );
+};
+
+export default Create;
